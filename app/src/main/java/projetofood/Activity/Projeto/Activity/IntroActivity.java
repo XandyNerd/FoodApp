088eb292a14d142175c27638b2207cd032eaa432
XandyNerdX <xandynerd@gmail.com>
2023-11-21 17:44:15 -0300

    FoodApp

---
+++ app/src/main/java/projetofood/Activity/Projeto/Activity/IntroActivity.java
@@ -0,0 +1,28 @@
+package projetofood.Activity.Projeto.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.foodappandroid.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ConstraintLayout starBtn=findViewById(R.id.startBtn);
+        starBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/projetofood/Activity/Projeto/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package projetofood.Activity.Projeto.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodappandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
